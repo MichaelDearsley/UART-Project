c9e9e9ad54dfd4d255202f42bb7eef8ef1a0cded
MichaelDearsley <alpacabob11@gmail.com>
2026-09-02 18:46:23 +0100

    Verified UART TX uart_tx_tb.sv

--- uart_tx_tb.sv
+++ uart_tx_tb.sv
@@ -6,29 +6,43 @@ module uart_tx_tb;
 
     logic clk;
     logic reset;
+    logic [7:0] tx_data;
+    logic tx_valid;
     logic tx;
 
     uart_tx dut (
         .clk(clk),
         .reset(reset),
+        .tx_data(tx_data),
+        .tx_valid(tx_valid),
         .tx(tx)
     );
 
     always #5 clk = ~clk;
 
-    initial begin
+    initial begin 
 
         $dumpfile("uart_tx.vcd");
         $dumpvars(0, uart_tx_tb);
 
         clk = 0;
         reset = 1;
+        tx_data = 8'h41; //Sends ASCII A
+        tx_valid = 0;
 
         #20;
 
         reset = 0;
 
-        #200;
+        #10;
+
+        tx_valid = 1;
+
+        #10;
+
+        tx_valid = 0;
+
+        #110
 
         $finish;
 
